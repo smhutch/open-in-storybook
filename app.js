eda588831d46f554ce0ff186439f5cca31c4a26f
Scott Hutcheson <scott@smhutch.co.uk>
2021-10-04 16:20:53 +0100

    upadte to main branch

--- app.js
+++ app.js
@@ -2,19 +2,27 @@ const express = require("express");
 
 const app = express();
 
-const BRANCH = "master";
+const BRANCH = "main";
 const PROJECT = "606de94e2208ee0021648fb9";
 const CHROMATIC_BASE_URL = `https://${BRANCH}--${PROJECT}.chromatic.com`;
 const PATH_QUERY = "?path=";
 
 const toChromaticUrl = (path) => `${CHROMATIC_BASE_URL}${PATH_QUERY}${path}`;
 
-const endpoints = [["/tokens", "/story/docs-tokens--page"]];
+const shortcuts = [
+  ["/tokens", "/story/docs-tokens--page"],
+  ["/themes", "/story/docs-themes--page"],
+  ["/palette", "/story/docs-palette--page"]
+];
 
-endpoints.forEach(([route, storybookPath]) => {
+shortcuts.forEach(([route, storybookPath]) => {
   app.get(route, (_req, res) => {
     res.redirect(toChromaticUrl(storybookPath));
   });
+});
+
+app.get("*", (req, res) => {
+  res.redirect(toChromaticUrl(req.url));
 });
 
 app.listen(8080, () => console.log("Listening on port 8080"));
